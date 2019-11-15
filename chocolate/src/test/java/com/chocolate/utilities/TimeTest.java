f269b1d86724b22f29b6ac7e7ef89a9a0af15372
Lucas <lucasperretta@gmail.com>
2019-11-15 15:06:49 -0300

    Added Unit Testing to the Chocolate Library

---
+++ chocolate/src/test/java/com/chocolate/utilities/TimeTest.java
@@ -0,0 +1,13 @@
+package com.chocolate.utilities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeTest {
+
+    @Test public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
