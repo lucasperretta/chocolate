bed52a55c6939300b4a0b4084e6d039f217175ff
Lucas <lucasperretta@gmail.com>
2019-12-11 17:09:52 -0300

    Added new Text Utilities

--- chocolate/src/main/java/com/chocolate/utilities/Text.java
+++ chocolate/src/main/java/com/chocolate/utilities/Text.java
@@ -1,15 +1,17 @@
 package com.chocolate.utilities;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Locale;
 
 @SuppressWarnings({"unused", "WeakerAccess", "RedundantSuppression"})
 public final class Text extends UtilityClass {
 
     // Constants.....
-    public static final int camelCase = 1;
-    public static final int snake_case = 2;
-    public static final int PascalCase = 3;
-    public static final int UPPER_SNAKE_CASE = 4;
+    private final static String REGEX_UPPER_CASE = ".*[A-Z]";
+    private final static String REGEX_LOWER_CASE = ".*[a-z]";
+    private final static String REGEX_NUMBERS = ".*[0-9]";
+    private final static String REGEX_SPECIAL_CHARACTERS = ".*[^a-zA-Z0-9]";
 
     // Methods.....
     public static String capitalizeFirstLetter(String string) {
@@ -25,11 +27,44 @@ public final class Text extends UtilityClass {
         return builder.toString().trim();
     }
 
-    // Classes.....
-    public static final class convert {
+    private static String buildRegex(@NotNull String repeat, int amount) {
+        StringBuilder regex = new StringBuilder();
+        for (int i = 0; i < amount; i++) {
+            regex.append(repeat);
+        }
+        return regex.toString() + ".*";
+    }
 
+    public static boolean hasAtLeastOneUpperCase(@NotNull String string) {
+        return string.matches(REGEX_UPPER_CASE + ".*");
+    }
 
+    public static boolean hasAtLeastOneLowerCase(@NotNull String string) {
+        return string.matches(REGEX_LOWER_CASE + ".*");
+    }
 
+    public static boolean hasAtLeastOneNumbers(@NotNull String string) {
+        return string.matches(REGEX_NUMBERS + ".*");
+    }
+
+    public static boolean hasAtLeastOneSpecialCharacters(@NotNull String string) {
+        return string.matches(REGEX_SPECIAL_CHARACTERS + ".*");
+    }
+
+    public static boolean hasAtLeastNAmountOfUpperCase(@NotNull String string, int amount) {
+        return string.matches(buildRegex(REGEX_UPPER_CASE, amount));
+    }
+
+    public static boolean hasAtLeastNAmountOfLowerCase(@NotNull String string, int amount) {
+        return string.matches(buildRegex(REGEX_LOWER_CASE, amount));
+    }
+
+    public static boolean hasAtLeastNAmountOfNumbers(@NotNull String string, int amount) {
+        return string.matches(buildRegex(REGEX_NUMBERS, amount));
+    }
+
+    public static boolean hasAtLeastNAmountOfSpecialCharacters(@NotNull String string, int amount) {
+        return string.matches(buildRegex(REGEX_SPECIAL_CHARACTERS, amount));
     }
 
 }
