fd09702ab6fc10ecb61e3513d5688f0f8717e40b
Lucas <lucasperretta@gmail.com>
2020-03-02 10:16:31 -0300

    Added the new Scheduled Utility Class

---
+++ chocolate/src/main/java/com/chocolate/utilities/Scheduled.java
@@ -0,0 +1,38 @@
+package com.chocolate.utilities;
+
+import androidx.annotation.NonNull;
+
+@SuppressWarnings({"unused", "WeakerAccess"}) public class Scheduled {
+
+    // Variables.....
+    protected Delayed delayed;
+    protected boolean cancelled = false;
+
+    // Constructor.....
+    public Scheduled(long interval, @NonNull Runnable runnable) {
+        this(interval, 0, runnable);
+    }
+
+    public Scheduled(long interval, long delay, @NonNull Runnable runnable) {
+        if (delay == 0) {
+            start(interval, runnable);
+        } else {
+            Delayed.run(delay, () -> start(interval, runnable));
+        }
+    }
+
+    // Methods.....
+    protected void start(long interval, @NonNull Runnable runnable) {
+        runnable.run();
+        delayed = Delayed.run(interval, () -> {
+            if (cancelled) return;
+            start(interval, runnable);
+        });
+    }
+
+    public void cancel() {
+        delayed.cancel();
+        cancelled = true;
+    }
+
+}
